772c64d0b985b59ffc89e162f62767c21c2654bf
jimbrian <ui.ux.jimbrian@gmail.com>
2022-08-24 15:31:24 +0300

    palindrome algorithms 2

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i=0; i<word.length/2; i++) {
+    const j = word.length -1-i;
+    if (word[i] !== word[j]) return false;
+    }
+return true;
 }
 
 /* 
   Add your pseudocode here
-*/
+ iterate from the start of the word to the middle
+ check each letter to the corresponding letter
+ if any letter don\`t match return false
 
 /*
   Add written explanation of your solution here
+ the code first iterates through from the beggining of the word to the middle
+ using a for loop and dividing it by half,then the code checks each lettter
+ to the corresponding letter from the end of the word. if any of the letter
+ doesn\`t match, it returns false, if it matches all the way through the middle
+ it returns true
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
